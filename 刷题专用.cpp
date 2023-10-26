cffb535f47ba8c4daf3ae8c598ae11ea96f7eb07
Leonlsyyy <145640095+lsyhsgggg@users.noreply.github.com>
2023-10-26 22:32:25 +0800

    Add files via upload

--- 刷题专用.cpp
+++ 刷题专用.cpp
@@ -1,40 +1,39 @@
 #include<iostream>
-#include<string>
-#include<algorithm>
 using namespace std;
-class gg {
-public:
-	string data;
-	int len;
-	int num;
-};//创建一个类，类的属性包括一个字符串存放数据，一个整数存放字符串长度（数据位数），还有一个整数存放对象在数组中下标
-gg arr[21];
-bool cmp(gg a,gg b) {
-	return a.data > b.data;
-}
-int main() {
+int main(){
+	int arr1[7];//存放中奖代码
+	int arr2[7];//存放奖票号码
+	int arr3[7] = {0};//存放中奖情况
 	int n;
 	cin >> n;
-	for (int i = 1; i <= n; i++) {
-		string ff;
-		cin >> ff;
-		arr[i].data = ff;
-		arr[i].len = ff.length();
-		arr[i].num = i;
-	}
-	int maxx = 0;
-	for (int i = 1; i <= n; i++) {
-		if (arr[i].len > maxx) { maxx = arr[i].len; }
-	}
-	for (int i = 1; i <= n; i++) {
-		for (int j = 0; j < (maxx - arr[i].len); j++) {
-			arr[i].data += "0";//将所有对象的数据末尾补零，补到与对象中最长字符串长度相同。
+	for (int i = 0; i < 7; i++) {
+		int j;
+		cin >> j;
+		arr1[i] = j;
+	}//存放中奖代码
+	
+	
+		for (int l = 0; l < n; l++) {
+			
+				for (int j = 0; j < 7; j++) {
+					int k;
+					cin >> k;
+					arr2[j] = k;
+				}//存放奖票号码
+			int num = 0;
+			for (int a = 0; a < 7; a++) {
+				for (int b = 0; b < 7; b++) {
+					if (arr1[a] == arr2[b]) {
+						num++;
+					}
+				}
+			}
+			if(num!=0) arr3[num - 1]++;
 		}
+	
+	
+	for (int i = 6; i >= 0; i--) {
+		cout << arr3[i] << " ";
 	}
-	sort(&arr[1], &arr[1] + n, cmp);//比较不同字符串的大小，从大到小排好序
-	for (int i = 1; i <= n; i++) {
-		for (int j = 0; j < arr[i].len; j++) {
-			cout << arr[i].data[j];
-		}
-		}
+	return 0;
 }
